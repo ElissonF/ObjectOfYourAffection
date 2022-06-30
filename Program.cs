d8770503dd19ba7585b1fde7450bfe4c3b502805
ElissonF <elissonfrancis@gmail.com>
2022-06-30 11:50:06 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using ObjectOfYourAffection;
+
+Profile _sam = new Profile("Sam Drakilla", 30, "NY", "USA", "he/him");
+_sam.SetHobbies(new string[] { "Ridding bikes", "Ridding Horses" });
+Console.WriteLine(_sam.ViewProfile());
